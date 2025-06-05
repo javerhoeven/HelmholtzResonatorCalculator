Fixed error in density calculation.

medium.py
@@ -12,7 +12,7 @@ class Medium():
         self.temperature_celsius = temperature
         self.temperature_kelvin = self.temperature_celsius + 273.15
         self.rel_humidity = rel_humidity
-        
+
         # check if optional input parameters are given
         if density is None:
             self.calc_density()
@@ -31,15 +31,27 @@ class Medium():
 
     def calc_density(self):
         # constants
-        p = 1013.15 # atmospheric pressure
+        p = 1013.15 * 100 # atmospheric pressure in Pa
         R_s = 287.1 # specific gas constant for dry air
         R_d = 461.5 # specific gas constant for wator vapor
         T = self.temperature_kelvin
         phi = self.rel_humidity
 
-        p_d = 6.112 * np.exp(17.62*T/(243.12 + T)) # vapor pressure according to wikipedia, valid between -45 and 60 °
-        R_f = R_s / (1-phi*(p_d / p) * (1-(R_s/R_d))) # gas constant considering air humidity
-        rho = p / (R_f*T) # density
+        # wiki approach
+        # p_d = 6.112 * np.exp(17.62*T/(243.12 + T)) # vapor pressure according to wikipedia, valid between -45 and 60 °
+        # R_f = R_s / (1-phi*(p_d / p) * (1-(R_s/R_d))) # gas constant considering air humidity
+        # rho = p / (R_f*T) # density
+
+        # TODO: check
+        # GPT approach
+        R_d = 287.05 # specific gas constant for dry air
+        R_v = 461.5 # specific gas constant for water vapor
+
+        p_sat = 6.112 * np.exp(17.62 * T / (243.12 * T)) * 100
+        p_v = phi * p_sat
+
+        rho = (p - p_v) / (R_d * T) + p_v / (R_v * T)
+
         self.density = rho
 
     def calc_speed_of_sound(self):

simulation.py
@@ -19,10 +19,6 @@ class Simulation():
         self.absorbtion_area : np.array = None
 
         
-
-    def run(self):
-        pass
-
     def calc_z_porous(self):
         """
         calculates the real, frequency-invariant porous absorbtion 
@@ -70,17 +66,19 @@ class Simulation():
         """
         calculates impedance based on acoustic stiffness and mass.
         becomes zero at resonance frequency
+
         """
 
         ap = self.resonator.aperture
         rho = self.sim_params.medium.density
         c = self.sim_params.medium.c
+        S = ap.area
         omega = self.sim_params.omega
         volume = self.resonator.geometry.volume
         l_ap = ap.length
         delta_l_in_out = ap.inner_end_correction + ap.outer_end_correction
       
-        self.z_stiff_mass = rho * c**2 / (1j*omega*volume) + 1j*omega*rho*(l_ap+delta_l_in_out)
+        self.z_stiff_mass = rho * c**2 / (1j*omega*volume) + 1j*omega*rho*(l_ap+delta_l_in_out) / S
 
     def calc_z_friction(self):
         """
@@ -128,7 +126,7 @@ class Simulation():
 
     def resonance_frequency(self) -> float:
         """returns the resonance frequency
-        calculated as minimum of the absorbtion area
+        calculated as maximum of the absorbtion area
 
         Returns:
             float: resonance frequency
@@ -137,7 +135,7 @@ class Simulation():
         if self.absorbtion_area is None:
             self.calc_absorbtion_area()
         
-        return self.sim_params.frequencies[np.argmin(self.absorbtion_area)]
+        return self.sim_params.frequencies[np.argmax(self.absorbtion_area)]
     def plot_absorbtion_area(self):
         """
         Plots the absorbtion area over the frequency