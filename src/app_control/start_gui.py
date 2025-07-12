618d62e012c945b002506ddc191203b785e5e3fd
jaanniis <jannis_v@hotmail.de>
2025-07-12 19:23:15 +0200

    Added poetry venv configuration for simpler installation process. Also finally added a .gitignore file.

--- src/app_control/start_gui.py
+++ src/app_control/start_gui.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt6.QtWidgets import  QApplication
-from gui_widgets.main_window import MainWindow
+# from gui_widgets.main_window import MainWindow
 
 def start_gui():
     app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
+    # TODO: fix
+    # win = MainWindow()
+    # win.show()
     sys.exit(app.exec())
